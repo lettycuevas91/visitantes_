b15e5817a85f10ac6f3a6144d809a2664d87f09a
Leticia <lettycuevas91@gmail.com>
2023-02-17 15:38:37 -0300

    visitantesss

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Visitor = require("./models/Visitor")
 app.set("view engine", "pug");
 app.set("views", "views")
 app.use(express.urlencoded({extended: true}));
-mongoose.set('strictQuery', true);
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true});
 mongoose.connection.on("error",function(e){console.error(e);});
 
