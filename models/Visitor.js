28a0d0fc5763792a53fd5b259a1e2f2f6556a509
Leticia <lettycuevas91@gmail.com>
2023-02-17 16:18:02 -0300

    visitantesss recurrentes

--- models/Visitor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = new mongoose.Schema({
-//  date:Date,
-  name:String,
-  count:Number
-});
-
-module.exports = mongoose.model("Visitor", schema);
